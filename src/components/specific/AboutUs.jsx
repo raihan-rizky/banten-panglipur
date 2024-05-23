9fbd5d8a77fa8988db715b9a2ce70f3f5ff46d4f
Raihan Rizki <raihanrizki71@gmail.com>
2024-05-23 11:54:17 +0700

    add About Us and fixed Hero

--- src/components/specific/AboutUs.jsx
+++ src/components/specific/AboutUs.jsx
@@ -3,8 +3,8 @@ import {ArrowRightIcon} from '@heroicons/react/24/solid'
 const AboutUs = () => {
     return(
         <>
-                <div className="overflow-hidden -translate-y-2 relative flex max-w-full min-h-[808px] bg-[url('./public/images/about-us-bg.png')]">
-                <div className='justify-between max-w-full min-h-[808px]'>
+                <div className="overflow-hidden xl:flex-col xl:items-center -translate-y-2 relative flex max-w-full min-h-[808px] bg-gradient-to-b from-[#87C0CD] to-white">
+                <div className='justify-between xl:flex-col max-w-full min-h-[808px]'>
                     <div className='max-w-full'>
                         <img className='absolute -translate-x-[650px] translate-y-[190px]' src='./public/images/ellipse-pattern.png'/>
                     </div>
@@ -12,7 +12,7 @@ const AboutUs = () => {
                         <img className='absolute rotate-180 max-w-full translate-x-[1000px] ' src='./public/images/ellipse-pattern.png'/>
                     </div>
                 </div>
-                    <div className="absolute translate-x-[144px] w-[498px] translate-y-[166px] max-w-full">
+                    <div className="container min-h-[808px] xl:ml-20 xl:p-0 xl:content-center pl-[149px] pb-[230px] absolute flex-col content-center xl:m-auto">
                         <h1 className="max-w-[392px] min-h-[68px] font-[700] text-[50px]">Tentang Kami</h1>
                         <p className="max-w-[498px] min-h-[200px] font-[400] text-[15px]">
                         Kami adalah penjaga kekayaan budaya yang tak ternilai di Banten. 
@@ -32,7 +32,7 @@ const AboutUs = () => {
                         </div>
                     </div>
 
-                    <div className='min-w-[567px] min-h-[371px] translate-x-[150px] translate-y-[166px] '>
+                    <div className='container z-20 pl-[250px] pb-[120px] xl:pl-0 xl:mr-auto mr-0 min-w-[567px] min-h-[371px] content-center '>
                         <img src='./public/images/about-us-img.png'/>
                     </div>
 
