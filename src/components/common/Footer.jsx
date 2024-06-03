4ce2e9de83009d68fd4fe66f9d65f8d125682489
Raihan Rizki <raihanrizki71@gmail.com>
2024-06-03 21:59:34 +0700

    fix footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,7 +4,7 @@
     return (
       <footer className="flex flex-col mt-0 items-center px-20 pt-10 pb-8 bg-[#5388AA] max-md:px-5">
         <header className="flex gap-5 justify-between items-center w-full text-base font-medium leading-6 max-w-[1166px] text-zinc-100 max-md:flex-wrap max-md:max-w-full">
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/d7477276369ec6f3262942f32bbf6e00762c43cc2fe8dc2d6dacd445535c0acd?apiKey=086f9e918f6c40cfbf2a7915a1bfe439&" alt="Logo" className="shrink-0 max-w-full aspect-[2.86] w-[129px]" />
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/d7477276369ec6f3262942f32bbf6e00762c43cc2fe8dc2d6dacd445535c0acd?apiKey=086f9e918f6c40cfbf2a7915a1bfe439&" alt="Logo" className="shrink-0 max-w-full aspect-[2.86] w-[129px] -translate-y-3" />
         <nav className="flex lg:grid-cols-2 lg:grid lg:gap-10 gap-20 self-stretch my-auto">
           <a href="#product" className="text-black">Product</a>{" "}
           <a href="#features" className="text-black">Features</a>{" "}
