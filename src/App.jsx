aa0291b71947e61d311bc7b6ec00212263b8c72f
Rohmadaw <rohmadalghifariw22@gmail.com>
2024-06-10 23:02:45 +0700

    add page ProfileWeb.jsx and compoment Deskripsi.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/common/Navbar'
 import Footer from './components/common/Footer'
-import Beranda from './pages/Beranda'
+import ProfileWeb from './pages/ProfileWeb'
+// import Beranda from './pages/Beranda'
 function App() {
   return (
     <>
           <NavBar></NavBar>
-          <Beranda></Beranda> 
+          {/* <Beranda></Beranda> */}
+          <ProfileWeb></ProfileWeb>
           <Footer></Footer>
           
     </>
