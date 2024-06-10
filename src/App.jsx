ce4a44a4333d15d8a96d5d94c015120c10e28183
Raihan Rizki <raihanrizki71@gmail.com>
2024-06-10 22:42:21 +0700

    add routing configuration with react-route-dom package

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import './App.css'
 import NavBar from './components/common/Navbar'
 import Footer from './components/common/Footer'
 import Beranda from './pages/Beranda'
+import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <>
           <NavBar></NavBar>
-          <Beranda></Beranda> 
+          <Routes>
+            <Route path="/" element = {<Beranda/>} />
+            <Route path="/profile" element = {<Beranda/>} />
+            <Route path="/wisata" element = {<Beranda/>} />
+            <Route path="/budaya" element = {<Beranda/>} />
+          </Routes>
           <Footer></Footer>
           
     </>
