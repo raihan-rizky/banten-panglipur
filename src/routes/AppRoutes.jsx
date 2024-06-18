99cfc0e53e10d8fc038c7cfbea63a0ea80a87838
Rohmadaw <rohmadalghifariw22@gmail.com>
2024-06-18 22:10:53 +0700

    solve import path

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import Beranda from '../views/pages/Beranda'
 import { Routes, Route } from 'react-router-dom';
 import IsiBudaya from '../views/templates/TemplateCreator';
-import ProfileWeb from '../pages/ProfileWeb';
+import ProfileWeb from '../views/pages/ProfileWeb';
 
 
 const AppRoutes = () => {
